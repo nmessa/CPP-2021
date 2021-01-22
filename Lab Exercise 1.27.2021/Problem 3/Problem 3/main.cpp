40bd8d1d6dbe47b5561716454c490fdddb3447cb
nmessa <nmessa@sau16.org>
2021-01-22 16:53:00 -0500

    Add files via upload

---
+++ Lab Exercise 1.27.2021/Problem 3/Problem 3/main.cpp
@@ -0,0 +1,31 @@
+//Lab Exercise 1/27/2021 Problem 3
+//Author: 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//Declare variables
+	int feet,  inches;
+
+	//Get input from user
+	//Add code here
+
+
+
+	//Make calculations
+	//Add code here
+
+
+	//Output the result
+	//Add code here
+
+
+	return 0;
+}
+
+//Output
+//Enter the number of feet: 6
+//Enter the number of inches: 3
+//The player is 75 inches tall
