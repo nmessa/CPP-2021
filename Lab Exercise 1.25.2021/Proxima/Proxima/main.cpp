f8af8e3ffce879c779faff26509a7809be0d6d1c
nmessa <nmessa@sau16.org>
2021-01-22 16:48:46 -0500

    Add files via upload

---
+++ Lab Exercise 1.25.2021/Proxima/Proxima/main.cpp
@@ -0,0 +1,31 @@
+//Lab Exercise 1.25.2021 Problem 3
+//Author: 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//Declare variables
+	double miles;
+	double lightyears = 4.22;
+	double lightspeed = 186000;
+	double secondsPerMinute = 60;
+	double minutesPerHour = 60;
+	double hoursPerDay = 24;
+	double daysPerYear = 365;
+
+	//Make calculation
+	//Add code here
+
+
+	//Output the answer
+	//Add code here
+
+
+	return 0;
+}
+
+//Sample Output
+//It is 2.47532e+013 miles to Proxima Cenauri
+//It takes 4.22 years for TV too reach Proxima Cenauri
---
+++ Lab Exercise 1.25.2021/Variables/Variables/main.cpp
@@ -0,0 +1,19 @@
+//Lab Exercise 1.25.2021 Problem 4
+//Author: 
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	//Add code here
+
+
+	return 0;
+}
+
+//Output
+//42
+//210
+//This is a string
