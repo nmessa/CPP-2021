87b3229fd22c2400e88ddb1d5eb0dc626496404b
nmessa <nmessa@sau16.org>
2021-01-22 16:51:06 -0500

    Add files via upload

---
+++ Lab Exercise 1.26.2021/Problem 1/Problem 1/main.cpp
@@ -0,0 +1,26 @@
+//Lab Exercise 1/26/2021 Problem 1
+//Author: 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//Declare variables
+	double totalSales = 4.6e6;
+	double percentSales = 0.62;
+	double eastSales;
+
+	//Make calculation
+	//Add code here
+
+
+	//Output the result
+	//Add code here
+
+
+	return 0;
+}
+
+//Output
+//East coast sales = 2.852e+006
