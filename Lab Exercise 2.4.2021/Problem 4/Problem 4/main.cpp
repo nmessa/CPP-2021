de710c4180c4a3f17797eab233d3cb4f179fa981
nmessa <nmessa@sau16.org>
2021-01-29 14:19:14 -0500

    Add files via upload

---
+++ Lab Exercise 2.4.2021/Problem 4/Problem 4/main.cpp
@@ -0,0 +1,33 @@
+//Lab Exercise 2/4/2021 Problem 4
+//Author: nmessa
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	//Declare and initialize variables
+	int numDays; 
+	double amount = 0.01, total = 0.0;
+
+	//Get days worked from user
+	//Add code here
+
+
+
+	//Calculate the amount of earnings
+	//Add code here
+
+
+	//Output to 2 decimal places
+	//Add code here
+
+
+
+	return 0;
+}
+
+//Output
+//How many days did the employee work? 30
+//Earnings for the month: $10737418.23
